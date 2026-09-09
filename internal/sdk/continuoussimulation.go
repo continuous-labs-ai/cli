6659de4ed587f8b75d26eb3d43839d36b51fdbb4
Minh <288481663+minh-continuous@users.noreply.github.com>
2026-09-09 00:02:40 +0000

    Prepare CLI version 0.1.0

--- internal/sdk/continuoussimulation.go
+++ internal/sdk/continuoussimulation.go
@@ -132,10 +132,10 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *ContinuousSimulation {
 	sdk := &ContinuousSimulation{
-		SDKVersion: "0.0.1",
+		SDKVersion: "0.1.0",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:         "speakeasy-sdk/go 0.0.1 2.935.1 v1 github.com/continuous-labs-ai/cli/internal/sdk",
-			SDKVersion:        "0.0.1",
+			UserAgent:         "speakeasy-sdk/go 0.1.0 2.935.1 v1 github.com/continuous-labs-ai/cli/internal/sdk",
+			SDKVersion:        "0.1.0",
 			GenVersion:        "2.935.1",
 			OpenAPIDocVersion: "v1",
 			ServerList:        ServerList,
