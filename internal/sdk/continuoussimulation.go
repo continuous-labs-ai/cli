76b4742db4d264b0ae24569d68ba5fcc2980576a
Minh Pham <288481663+minh-continuous@users.noreply.github.com>
2026-09-18 02:28:00 +0000

    ci: regenerated with OpenAPI Doc , Speakeasy CLI 1.796.4 (#32)
    Co-authored-by: speakeasybot <bot@speakeasyapi.dev>

--- internal/sdk/continuoussimulation.go
+++ internal/sdk/continuoussimulation.go
@@ -132,10 +132,10 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *ContinuousSimulation {
 	sdk := &ContinuousSimulation{
-		SDKVersion: "0.1.11",
+		SDKVersion: "0.1.12",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:         "speakeasy-sdk/go 0.1.11 2.935.1 v1 github.com/continuous-labs-ai/cli/internal/sdk",
-			SDKVersion:        "0.1.11",
+			UserAgent:         "speakeasy-sdk/go 0.1.12 2.935.1 v1 github.com/continuous-labs-ai/cli/internal/sdk",
+			SDKVersion:        "0.1.12",
 			GenVersion:        "2.935.1",
 			OpenAPIDocVersion: "v1",
 			ServerList:        ServerList,
